01ebc91873374366d4bd2d4f3def5f1cc0f4b46c
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-17 20:13:16 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -5,6 +5,7 @@ class Teacher < User
 
   knowledge = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async we...instance variable", "programming computers hacking learning terminal
   ", "bash Ruby rvm update certs"]
+  
   def teach
     knowledge.sample
    end
