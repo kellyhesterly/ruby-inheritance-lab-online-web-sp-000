0b8d7dc2bc456e4dc342ff711c17c64d0d039bb1
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-17 20:08:16 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -4,7 +4,8 @@ require_relative "./user.rb"
 class Teacher < User
 
   def teach
-    puts ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async we...instance variable", "programming computers hacking learning terminal
+    knowledge = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async we...instance variable", "programming computers hacking learning terminal
     ", "bash Ruby rvm update certs"]
+    puts knowledge
    end
 end
